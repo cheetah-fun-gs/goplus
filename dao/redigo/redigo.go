6a093d0f7eff7b75ac21b094d401d075e443ed9c
张劲 <zhangjin@zhangjindeMacBook-Pro.local>
2019-01-16 12:25:30 +0800

    fix:优化

--- dao/redigo/redigo.go
+++ dao/redigo/redigo.go
@@ -6,11 +6,17 @@ import (
 	redigo "github.com/gomodule/redigo/redis"
 )
 
+// ErrorLocked 被占用
+var ErrorLocked = fmt.Errorf("locked")
+
 // Lock redis锁 秒级 只锁不解
-func Lock(conn redigo.Conn, lockKey string, timeOut int) (bool, error) {
+func Lock(conn redigo.Conn, lockKey string, timeOut int) error {
 	value, err := redigo.String(conn.Do("SET", lockKey, "", "EX", fmt.Sprintf("%d", timeOut), "NX"))
 	if err != nil {
-		return false, err
+		return err
 	}
-	return value == "OK", nil
+	if value != "OK" {
+		return ErrorLocked
+	}
+	return nil
 }
