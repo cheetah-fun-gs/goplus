2e04f572a68dfc6b83cf51ad7f25b273185e03f0
zhangjin <zhangjin@cmcm.com>
2019-04-23 15:01:45 +0800

    perf:对 redigo 的get set 做一个封装

--- dao/redigo/redigo.go
+++ dao/redigo/redigo.go
@@ -1,6 +1,7 @@
 package redigo
 
 import (
+	"encoding/json"
 	"fmt"
 
 	redigo "github.com/gomodule/redigo/redis"
@@ -19,4 +20,39 @@ func Lock(conn redigo.Conn, lockKey string, timeOut int) error {
 		return ErrorLocked
 	}
 	return nil
+}
+
+// Set Set
+func Set(conn redigo.Conn, key string, value interface{}, timeOut int) error {
+	storeValue := ""
+	if value != nil {
+		data, err := json.Marshal(value)
+		if err != nil {
+			return err
+		}
+		storeValue = string(data)
+	}
+	_, err := conn.Do("SET", key, storeValue, "EX", timeOut)
+	return err
+}
+
+// Get Get
+func Get(conn redigo.Conn, key string, value interface{}) (bool, error) {
+	storeValue, err := redigo.String(conn.Do("GET", key))
+	if err != nil && err != redigo.ErrNil {
+		return false, err
+	}
+	if err == redigo.ErrNil { // 找不到 返回空
+		return false, nil
+	}
+
+	if storeValue == "" {
+		return true, nil
+	}
+
+	err = json.Unmarshal([]byte(storeValue), value)
+	if err != nil {
+		return false, err
+	}
+	return true, nil
 }
