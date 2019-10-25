bfeede8dc293be6474e07b60ce2877fa6baf826e
zhangjin <zhangjin@cmcm.com>
2019-10-25 22:09:43 +0800

    perf:优化

--- multier/multilogger/wrapper.go
+++ multier/multilogger/wrapper.go
@@ -2,24 +2,34 @@ package multilogger
 
 import "context"
 
+// 注意 非N 方法不能引用 N 方法, 会造成 caller depth 不一致
+
 // Debug Debug
 func Debug(ctx context.Context, format string, v ...interface{}) {
-	DebugN(ctx, d, format, v...)
+	if logger, ok := mutil[d]; ok {
+		logger.Debug(ctx, format, v...)
+	}
 }
 
 // Info Info
 func Info(ctx context.Context, format string, v ...interface{}) {
-	InfoN(ctx, d, format, v...)
+	if logger, ok := mutil[d]; ok {
+		logger.Info(ctx, format, v...)
+	}
 }
 
 // Warn Warn
 func Warn(ctx context.Context, format string, v ...interface{}) {
-	WarnN(ctx, d, format, v...)
+	if logger, ok := mutil[d]; ok {
+		logger.Warn(ctx, format, v...)
+	}
 }
 
 // Error Error
 func Error(ctx context.Context, format string, v ...interface{}) {
-	ErrorN(ctx, d, format, v...)
+	if logger, ok := mutil[d]; ok {
+		logger.Error(ctx, format, v...)
+	}
 }
 
 // DebugN Debug with name
