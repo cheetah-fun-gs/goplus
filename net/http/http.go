33b8d00489e06ace7d9ae443e0c74addf70e1395
zhangjin <zhangjin@cmcm.com>
2019-11-07 21:40:33 +0800

    perf:修复defer导致的问题

--- net/http/http.go
+++ net/http/http.go
@@ -132,7 +132,6 @@ type MultipartFormField struct {
 func (client *Client) JSONMultipartForm(url string, fields []*MultipartFormField, response interface{}) error {
 	bodyBuf := &bytes.Buffer{}
 	bodyWriter := multipart.NewWriter(bodyBuf)
-	defer bodyWriter.Close()
 
 	for _, field := range fields {
 		if field.FilePath == "" {
@@ -163,6 +162,7 @@ func (client *Client) JSONMultipartForm(url string, fields []*MultipartFormField
 	}
 
 	contentType := bodyWriter.FormDataContentType()
+	bodyWriter.Close()
 
 	resp, err := client.httpClient().Post(url, contentType, bodyBuf)
 	if err != nil {
