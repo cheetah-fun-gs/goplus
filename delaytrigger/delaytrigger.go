4acb991974f21027e48146ef8f518eecf1e5fada
zhangjin <zhangjin@cmcm.com>
2019-12-20 23:25:32 +0800

    perf:优化事件获取方式

--- delaytrigger/delaytrigger.go
+++ delaytrigger/delaytrigger.go
@@ -49,66 +49,106 @@ func (trigger *DelayTrigger) getEventKey(eventID string) string {
 	return fmt.Sprintf("%s:%s:delaytriggerevent", trigger.name, eventID)
 }
 
-// GetEvents 获取所有事件信息
-func (trigger *DelayTrigger) GetEvents() ([]*Event, error) {
+func isMatchID(id string, ids []string) bool {
+	if len(ids) == 0 {
+		return true
+	}
+	for _, i := range ids {
+		if id == i {
+			return true
+		}
+	}
+	return false
+}
+
+func isMathStatus(status EventStatus, statuses []EventStatus) bool {
+	if len(statuses) == 0 {
+		return true
+	}
+	for _, s := range statuses {
+		if status == s {
+			return true
+		}
+	}
+	return false
+}
+
+// GetEventsWithParam 获取指定事件 ids statuses 为空表示 全匹配
+func (trigger *DelayTrigger) GetEventsWithParam(ids []string, statuses []EventStatus) ([]*Event, error) {
 	conn := trigger.pool.Get()
 	defer conn.Close()
 
 	key := trigger.getTriggerKey()
-	rs := []*Event{}
-	if err := redigoplus.HVals(conn, key, &rs); err != nil {
-		return nil, err
-	}
-	return rs, nil
-}
-
-// GetActivedEvents 获取已激活的所有事件信息
-func (trigger *DelayTrigger) GetActivedEvents() ([]*Event, error) {
-	events, err := trigger.GetEvents()
-	if err != nil {
-		return nil, err
+	events := []*Event{}
+	if len(ids) == 0 {
+		if err := redigoplus.HVals(conn, key, &events); err != nil {
+			return nil, err
+		}
+	} else {
+		v := map[string]interface{}{}
+		for _, i := range ids {
+			v[i] = &Event{}
+		}
+		if err := redigoplus.HMGet(conn, key, v); err != nil {
+			return nil, err
+		}
+		for _, vv := range v {
+			events = append(events, vv.(*Event))
+		}
 	}
 
 	rs := []*Event{}
 	for _, event := range events {
-		if event.Status == EventStatusActived {
+		if isMathStatus(event.Status, statuses) {
 			rs = append(rs, event)
 		}
 	}
 	return rs, nil
 }
 
-// EventWithCount 事件信息和目标数
-type EventWithCount struct {
+// EventAndCount 事件信息和目标数
+type EventAndCount struct {
 	*Event
 	Count int
 }
 
-// GetEventWithCounts 获取所有事件信息
-func (trigger *DelayTrigger) GetEventWithCounts() ([]*EventWithCount, error) {
-	events, err := trigger.GetEvents()
+// GetEventsAndCountsWithParam 获取所有事件信息
+func (trigger *DelayTrigger) GetEventsAndCountsWithParam(ids []string, statuses []EventStatus) (int, []*EventAndCount, error) {
+	events, err := trigger.GetEventsWithParam(ids, statuses)
 	if err != nil {
-		return nil, err
+		return 0, nil, err
 	}
-	return trigger.GetCountForEvents(events)
+
+	newIDS := []string{}
+	for _, event := range events {
+		newIDS = append(newIDS, event.ID)
+	}
+
+	counts, err := trigger.getTargetCounts(newIDS)
+	if err != nil {
+		return 0, nil, err
+	}
+
+	total := 0
+	rs := []*EventAndCount{}
+	for i := 0; i < len(events); i++ {
+		total += counts[i]
+		rs = append(rs, &EventAndCount{
+			Event: events[i],
+			Count: counts[i],
+		})
+	}
+
+	return total, rs, nil
 }
 
-// GetActivedEventWithCounts 获取已激活的所有事件信息
-func (trigger *DelayTrigger) GetActivedEventWithCounts() ([]*EventWithCount, error) {
-	events, err := trigger.GetActivedEvents()
-	if err != nil {
-		return nil, err
-	}
-	return trigger.GetCountForEvents(events)
-}
-
-// GetCountForEvents 获取事件数量
-func (trigger *DelayTrigger) GetCountForEvents(events []*Event) ([]*EventWithCount, error) {
+// getTargetCounts 获取事件的目标数
+func (trigger *DelayTrigger) getTargetCounts(ids []string) ([]int, error) {
 	conn := trigger.pool.Get()
 	defer conn.Close()
 
-	for _, event := range events {
-		if err := conn.Send("SCARD", trigger.getEventKey(event.ID)); err != nil {
+	for _, id := range ids {
+		if err := conn.Send("SCARD", trigger.getEventKey(id)); err != nil {
 			return nil, err
 		}
 	}
@@ -116,80 +156,35 @@ func (trigger *DelayTrigger) GetCountForEvents(events []*Event) ([]*EventWithCou
 		return nil, err
 	}
 
-	rs := []*EventWithCount{}
-	for _, event := range events {
+	rs := []int{}
+	for i := 0; i < len(ids); i++ {
 		count, err := redigo.Int(conn.Receive())
 		if err != nil {
 			return nil, err
 		}
-		rs = append(rs, &EventWithCount{
-			Event: event,
-			Count: count,
-		})
+		rs = append(rs, count)
 	}
 	return rs, nil
 }
 
-// GetTargetCount 获得所有目标数
-func (trigger *DelayTrigger) GetTargetCount() (int, error) {
-	eventWithCounts, err := trigger.GetEventWithCounts()
-	if err != nil {
-		return 0, err
-	}
-	total := 0
-	for _, e := range eventWithCounts {
-		total += e.Count
-	}
-	return total, nil
+// 以下为快捷方式
+
+// GetEvents 获得所有事件
+func (trigger *DelayTrigger) GetEvents() ([]*Event, error) {
+	return trigger.GetEventsWithParam([]string{}, []EventStatus{})
 }
 
-// GetActivedTargetCount 获得活跃的目标数
-func (trigger *DelayTrigger) GetActivedTargetCount() (int, error) {
-	eventWithCounts, err := trigger.GetActivedEventWithCounts()
-	if err != nil {
-		return 0, err
-	}
-	total := 0
-	for _, e := range eventWithCounts {
-		total += e.Count
-	}
-	return total, nil
+// GetActivedEvents 获得所有活跃事件
+func (trigger *DelayTrigger) GetActivedEvents() ([]*Event, error) {
+	return trigger.GetEventsWithParam([]string{}, []EventStatus{EventStatusActived})
 }
 
-// GetTargetCountForEventIDS 获得所有目标数
-func (trigger *DelayTrigger) GetTargetCountForEventIDS(eventIDS []string) (int, error) {
-	events := []*Event{}
-	for _, eID := range eventIDS {
-		events = append(events, &Event{
-			ID: eID,
-		})
-	}
-
-	eventWithCounts, err := trigger.GetCountForEvents(events)
-	if err != nil {
-		return 0, err
-	}
-	total := 0
-	for _, e := range eventWithCounts {
-		total += e.Count
-	}
-	return total, nil
+// GetEventsAndCounts  获得所有事件 和 目标数
+func (trigger *DelayTrigger) GetEventsAndCounts() (int, []*EventAndCount, error) {
+	return trigger.GetEventsAndCountsWithParam([]string{}, []EventStatus{})
 }
 
-// GetActivedTargetCountForEventIDS 获得活跃的目标数
-func (trigger *DelayTrigger) GetActivedTargetCountForEventIDS(eventIDS []string) (int, error) {
-	eventWithCounts, err := trigger.GetActivedEventWithCounts()
-	if err != nil {
-		return 0, err
-	}
-
-	total := 0
-	for _, e := range eventWithCounts {
-		for _, eID := range eventIDS {
-			if eID == e.ID {
-				total += e.Count
-			}
-		}
-	}
-	return total, nil
+// GetActivedEventsAndCounts  获得所有活跃事件 和 目标数
+func (trigger *DelayTrigger) GetActivedEventsAndCounts() (int, []*EventAndCount, error) {
+	return trigger.GetEventsAndCountsWithParam([]string{}, []EventStatus{EventStatusActived})
 }
