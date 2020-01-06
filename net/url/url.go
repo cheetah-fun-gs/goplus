8423d8752693f70f122c874d30ddfa9e5b49d36e
zhangjin <zhangjin@cmcm.com>
2020-01-06 11:03:32 +0800

    perf:新增ToRawQueryEncoded

--- net/url/url.go
+++ net/url/url.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/url"
+	"sort"
 	"strconv"
+	"strings"
 )
 
 func toString(v interface{}) string {
@@ -44,11 +46,41 @@ func ToValues(v interface{}) (url.Values, error) {
 	return values, nil
 }
 
+// ToRawQueryEncoded 转换成 URL.RawQuery的urlencode格式; 不要使用浮点数
+func ToRawQueryEncoded(v interface{}) (string, error) {
+	values, err := ToValues(v)
+	if err != nil {
+		return "", err
+	}
+	return values.Encode(), nil
+}
+
 // ToRawQuery 转换 URL.RawQuery; 不要使用浮点数
 func ToRawQuery(v interface{}) (string, error) {
 	values, err := ToValues(v)
 	if err != nil {
 		return "", err
 	}
-	return values.Encode(), nil
+	// 改 values.Encode()
+	if values == nil {
+		return "", nil
+	}
+	var buf strings.Builder
+	keys := make([]string, 0, len(values))
+	for k := range values {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	for _, key := range keys {
+		vs := values[key]
+		for _, vv := range vs {
+			if buf.Len() > 0 {
+				buf.WriteByte('&')
+			}
+			buf.WriteString(key)
+			buf.WriteByte('=')
+			buf.WriteString(vv)
+		}
+	}
+	return buf.String(), nil
 }
